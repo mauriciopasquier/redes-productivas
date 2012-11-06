09c8605ea4cb031d0bcdcbf5064a9a670bf08f1e
Mauricio Pasquier Juan <mauricio@pasquierjuan.com.ar>
2012-11-06 10:53:54 -0300

    Grafo en la vista de cadenas.

--- app/decorators/producto_decorator.rb
+++ app/decorators/producto_decorator.rb
@@ -4,8 +4,9 @@ class ProductoDecorator < Draper::Base
   # Devuelve el label para .dot
   def label
     html =  "<div>"
-    html +=   "<img  alt='#{h.image_alt 'fugue-icons/wooden-box.png'}' src='#{h.image_path 'fugue-icons/wooden-box.png'}' />"
-    html +=   "<a class='menu' href='#'>#{nombre}</a>"
+    html +=   "<img  alt='#{h.image_alt 'fugue-icons/wooden-box.png'}'"
+    html +=         "src='#{h.image_path 'fugue-icons/wooden-box.png'}' />"
+    html +=   "<a class='menu' href='#{h.url_for(producto)}'>#{nombre}</a>"
     html += "</div>"
   end
 
--- app/models/actividad.rb
+++ app/models/actividad.rb
@@ -4,11 +4,11 @@ class Actividad < ActiveRecord::Base
   belongs_to :ciiu
   delegate :codigo, to: :ciiu
 
-  has_many :origenes, class_name: 'Relacion', as: :salida
+  has_many :origenes, class_name: 'Relacion', as: :salida, dependent: :destroy
   has_many :entradas, through: :origenes,
             source: :entrada, source_type: 'Producto'
 
-  has_many :destinos, class_name: 'Relacion', as: :entrada
+  has_many :destinos, class_name: 'Relacion', as: :entrada, dependent: :destroy
   has_many :salidas, through: :destinos,
             source: :salida, source_type: 'Producto'
 
